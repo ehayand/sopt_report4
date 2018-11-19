5b223ce9c5c0969388d9cea873b1d2bf221d28ff
Jaeyu Jo <ehay@naver.com>
2018-11-19 14:56:31 +0900

    S.O.P.T 23rd Server part report3 Init

---
+++ src/main/java/org/sopt/report4/Report4Application.java
@@ -0,0 +1,12 @@
+package org.sopt.report4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Report4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Report4Application.class, args);
+    }
+}
